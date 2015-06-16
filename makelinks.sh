43eda8a3671ec5ccc78fbcbcff0026eb02ecf34c
Eigil Skjæveland <eigil@skjvlnd.com>
2015-06-16 18:36:16 +0200

    Restructured stuff

--- makelinks.sh
+++ makelinks.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-for DOTFILE in $(find . -type f)
+for DOTFILE in $(find files -type f)
 do
     DOTFILE=${DOTFILE#./}
-    [ $DOTFILE != `basename $0` ] &&
-        ([ -e ~/.$DOTFILE ] || install -D /dev/null ~/.$DOTFILE) &&
+    ([ -e ~/.$DOTFILE ] || install -D /dev/null ~/.$DOTFILE) &&
         ln -sb $PWD/$DOTFILE ~/.$DOTFILE &&
         echo "Created link ~/.$DOTFILE -> $PWD/$DOTFILE"
 done
