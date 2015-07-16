c22a2ff40aaaef4a4d325112417c22d7e5f87a5d
Eigil Skjæveland <eigil@skjvlnd.com>
2015-07-16 15:21:52 +0200

    Årna urxvt og dwm

--- makelinks.sh
+++ makelinks.sh
@@ -4,6 +4,6 @@ for DOTFILE in $(find files -type f)
 do
     DOTFILE=${DOTFILE#./}
     ([ -e ~/.$DOTFILE ] || install -D /dev/null ~/.$DOTFILE) &&
-        ln -sb $PWD/$DOTFILE ~/.$DOTFILE &&
-        echo "Created link ~/.$DOTFILE -> $PWD/$DOTFILE"
+        ln -sb $PWD/$DOTFILE ~/.${DOTFILE#files/} &&
+        echo "Created link ~/.${DOTFILE#files/} -> $PWD/$DOTFILE"
 done
