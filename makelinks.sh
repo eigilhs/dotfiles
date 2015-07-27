173116cfe316988acd47fdcef47302d6ddd763ab
Eigil Skjæveland <eigil@skjvlnd.com>
2015-07-27 13:46:20 +0200

    Fixed bug in link script

--- makelinks.sh
+++ makelinks.sh
@@ -2,8 +2,7 @@
 
 for DOTFILE in $(find files -type f)
 do
-    DOTFILE=${DOTFILE#./}
-    ([ -e ~/.$DOTFILE ] || install -D /dev/null ~/.$DOTFILE) &&
+    ([ -e ~/.${DOTFILE#files/} ] || install -D /dev/null ~/.${DOTFILE#files/}) &&
         ln -sb $PWD/$DOTFILE ~/.${DOTFILE#files/} &&
         echo "Created link ~/.${DOTFILE#files/} -> $PWD/$DOTFILE"
 done
