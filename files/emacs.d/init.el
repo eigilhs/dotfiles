4a87cbf906e3f122caf65cf21300e0ce4574d5a8
Eigil Skjæveland <eigil@skjaeve.land>
2016-04-16 02:21:40 +0200

    Updates

--- files/emacs.d/init.el
+++ files/emacs.d/init.el
@@ -87,12 +87,8 @@
 ;; Use (setq ...) to set value locally to a buffer
 ;; Use (setq-default ...) to set value globally
 (setq-default indent-tabs-mode nil)
-
-;; Go bits. 
-;;(require 'go-mode-load)
-;;(add-hook 'before-save-hook #'gofmt-before-save)
-;;(add-hook 'go-mode-hook (lambda () (setq indent-tabs-mode nil)))
-;;(add-hook 'go-mode-hook (lambda () (setq tab-width 4)))
+(setq c-default-style "linux")
+(add-hook 'c-mode-hook (lambda () (setq indent-tabs-mode t)))
 
 (defun set-frame-size-according-to-resolution ()
   (interactive)
@@ -174,9 +170,6 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 
 (setq asm-comment-char ?\#)
-(setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60
-                      64 68 72 76 80 84 88 92 96 100 104 108 112
-                      116 120))
 
 ;; simple regexp used to check the message. Tweak to your own need.
 ;; (defvar my-message-attachment-regexp "\\([Ww]e send\\|[Ii] send\\|attach\\)")
