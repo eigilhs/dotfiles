8b3e7c6bb6852292c779a1deae3d628be5443dcc
Eigil Skjæveland <eigil@skjaeve.land>
2021-11-07 15:14:25 +0100

    Simplify emacs config

--- files/emacs.d/init.el
+++ files/emacs.d/init.el
@@ -1,277 +1,32 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
- '(compilation-message-face (quote default))
- '(custom-enabled-themes (quote (github)))
- '(custom-safe-themes
-   (quote
-    ("c567c85efdb584afa78a1e45a6ca475f5b55f642dfcd6277050043a568d1ac6f" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "003a9aa9e4acb50001a006cfde61a6c3012d373c4763b48ceb9d523ceba66829" "73a13a70fd111a6cd47f3d4be2260b1e4b717dbf635a9caee6442c949fad41cd" "228c0559991fb3af427a6fa4f3a3ad51f905e20f481c697c6ca978c5683ebf43" "de0b7245463d92cba3362ec9fe0142f54d2bf929f971a8cdf33c0bf995250bcf" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "7feeed063855b06836e0262f77f5c6d3f415159a98a9676d549bfeb6c49637c4" "ecb9fe1d5b165a35499191a909b2b5710a52935614058b327a39bfbbb07c7dc8" "c1fb68aa00235766461c7e31ecfc759aa2dd905899ae6d95097061faeb72f9ee" "357d5abe6f693f2875bb3113f5c031b7031f21717e8078f90d9d9bc3a14bcbd8" "0f97285f9e0c7d9cad04f2130859d20d6c9b3142877b2bca52d958f4f1cf346f" "d6e27431f8cafb4a9136aebb1d4061f895b120bf88d34ff60c390d9905bd4e36" "57f8801351e8b7677923c9fe547f7e19f38c99b80d68c34da6fa9b94dc6d3297" "97a2b10275e3e5c67f46ddaac0ec7969aeb35068c03ec4157cf4887c401e74b1" "0e121ff9bef6937edad8dfcff7d88ac9219b5b4f1570fd1702e546a80dba0832" "941bc214a26ed295e68bbeaadcd279475a3d6df06ae36b0b2872319d58b855f7" default)))
- '(fci-rule-color "#3C3D37" t)
- '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
- '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
- '(highlight-tail-colors
-   (quote
-    (("#3C3D37" . 0)
-     ("#679A01" . 20)
-     ("#4BBEAE" . 30)
-     ("#1DB4D0" . 50)
-     ("#9A8F21" . 60)
-     ("#A75B00" . 70)
-     ("#F309DF" . 85)
-     ("#3C3D37" . 100))))
- '(inhibit-startup-screen t)
- '(magit-diff-use-overlays nil)
- '(nrepl-message-colors
-   (quote
-    ("#183691" "#969896" "#a71d5d" "#969896" "#0086b3" "#795da3" "#a71d5d" "#969896")))
- '(package-selected-packages
-   (quote
-    (ponylang-mode scala-mode go-mode emr graphviz-dot-mode latex-extra outline-magic web-mode tango-plus-theme slime slack rust-mode perl6-mode pdf-tools nim-mode nhexl-mode multiple-cursors monokai-theme markdown-mode magit hemisu-theme github-theme fill-column-indicator espresso-theme dart-mode d-mode cypher-mode color-theme-sanityinc-tomorrow clojure-mode base16-theme airline-themes)))
- '(pdf-view-midnight-colors (quote ("#969896" . "#f8eec7")))
- '(pos-tip-background-color "#FFFACE")
- '(pos-tip-foreground-color "#272822")
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#F92672")
-     (40 . "#CF4F1F")
-     (60 . "#C26C0F")
-     (80 . "#E6DB74")
-     (100 . "#AB8C00")
-     (120 . "#A18F00")
-     (140 . "#989200")
-     (160 . "#8E9500")
-     (180 . "#A6E22E")
-     (200 . "#729A1E")
-     (220 . "#609C3C")
-     (240 . "#4E9D5B")
-     (260 . "#3C9F79")
-     (280 . "#A1EFE4")
-     (300 . "#299BA6")
-     (320 . "#2896B5")
-     (340 . "#2790C3")
-     (360 . "#66D9EF"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
-;;(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
-;; If there is more than one, they won't work right.
-;;)
-
-
-;; ===== Automatically load abbreviations table =====
-;; Note that emacs chooses, by default, the filename
-;; "~/.abbrev_defs", so don't try to be too clever
-;; by changing its name
-
-(scroll-bar-mode -1)
 (when window-system
+  (load-theme 'wombat t)
+  (scroll-bar-mode -1)
   (tooltip-mode -1)
-  (tool-bar-mode -1))
+  (tool-bar-mode -1)
+  (global-linum-mode 1)
+  (setq linum-format "%d "
+	frame-title-format (list "%b %f")))
+
+(setq inhibit-startup-screen t
+      scroll-step 1
+      scroll-margin 2
+      sentence-end-double-space nil
+      c-default-style "linux"
+      custom-file (expand-file-name "custom.el" user-emacs-directory))
+
 (menu-bar-mode -1)
-
-(setq-default abbrev-mode t)
-(read-abbrev-file "~/.emacs.d/abbrev_defs")
-(setq save-abbrevs t)
-
-;; ===== Set the highlight current line minor mode =====
-;;(global-hl-line-mode 1)
-
-;; ========== Line by line scrolling ==========
-(setq scroll-step 1)
-
-;; ===== Set standard indent to 2 rather that 4 ====
-(setq standard-indent 4)
-
-;; ========== Place Backup Files in Specific Directory ==========
-;; Enable backup files.
-(setq make-backup-files t)
-;; Enable versioning with default values (keep five last versions, I think!)
-(setq version-control t)
-;; Save all backup file in this directory.
-(setq backup-directory-alist (quote ((".*" . "/tmp/"))))
-
-;; ========== Enable Line and Column Numbering ==========
-;; Show line-number in the mode line
 (line-number-mode 1)
-;; Show column-number in the mode line
 (column-number-mode 1)
-
-(setq tramp-default-method "rsync")
-
-(fset 'yes-or-no-p 'y-or-n-p)
 (show-paren-mode t)
-(global-linum-mode 1)
-(setq linum-format "%d ")
+(fset 'yes-or-no-p 'y-or-n-p)
 
+(setq backup-directory-alist `(("." . ,(expand-file-name "backups" user-emacs-directory)))
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
 
-(autoload 'python-mode "python" nil t)
-
-(setq browse-url-browser-function 'browse-url-chromium)
-(setq-default frame-title-format (list "%b %f"))
-
-(require 'whitespace)
-(global-set-key "\C-c_w" 'whitespace-mode)
-(prefer-coding-system      'utf-8)
-(add-to-list 'load-path "~/emacs.d/")
-
-;; ===== Turn off tab character =====
-;; Use (setq ...) to set value locally to a buffer
-;; Use (setq-default ...) to set value globally
-(setq-default indent-tabs-mode nil)
-(setq c-default-style "linux")
-(add-hook 'c-mode-hook (lambda () (setq indent-tabs-mode t)))
-
-(defun set-frame-size-according-to-resolution ()
-  (interactive)
-  (if window-system
-  (progn
-    ;; use 120 char wide window for largeish displays
-    ;; and smaller 80 column windows for smaller displays
-    ;; pick whatever numbers make sense for you
-;;    (if (> (x-display-pixel-width) 1280)
-;;           (add-to-list 'default-frame-alist (cons 'width 120))
-;;           (add-to-list 'default-frame-alist (cons 'width 80)))
-    ;; for the height, subtract a couple hundred pixels
-    ;; from the screen height (for panels, menubars and
-    ;; whatnot), then divide by the height of a char to
-    ;; get the height we want
-    (add-to-list 'default-frame-alist 
-         (cons 'height (/ (- (x-display-pixel-height) 100)
-                             (frame-char-height)))))))
-
-(add-hook 'c-mode-hook 'fci-mode)
-(add-hook 'python-mode-hook 'fci-mode)
-(setq fci-rule-color "#444")
-(setq fci-rule-column 72)
-
-(add-to-list 'load-path "~/.emacs.d/plugins/")
-
-(require 'package)
 (add-to-list 'package-archives
-  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  '("melpa" . "https://melpa.org/packages/") t)
 
-(package-initialize)
-(load-theme 'monokai t)
-
-(require 'multiple-cursors)
-;; Experimental multiple-cursors
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
-(global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
-;; Mark additional regions matching current region
-(global-set-key (kbd "M-æ") 'mc/mark-all-dwim)
-(global-set-key (kbd "C-å") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-æ") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-Æ") 'mc/mark-more-like-this-extended)
-(global-set-key (kbd "M-å") 'mc/mark-all-in-region)
-;; Symbol and word specific mark-more
-(global-set-key (kbd "s-æ") 'mc/mark-next-word-like-this)
-(global-set-key (kbd "s-å") 'mc/mark-previous-word-like-this)
-(global-set-key (kbd "M-s-æ") 'mc/mark-all-words-like-this)
-(global-set-key (kbd "s-Æ") 'mc/mark-next-symbol-like-this)
-(global-set-key (kbd "s-Å") 'mc/mark-previous-symbol-like-this)
-(global-set-key (kbd "M-s-Æ") 'mc/mark-all-symbols-like-this)
-;; Extra multiple cursors stuff
-(global-set-key (kbd "C-~") 'mc/reverse-regions)
-(global-set-key (kbd "M-~") 'mc/sort-regions)
-(global-set-key (kbd "C-#") 'mc/insert-numbers)
-(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
-;; Set anchor to start rectangular-region-mode
-(global-set-key (kbd "C- ") 'set-rectangular-region-anchor)
-
-
-(set-frame-size-according-to-resolution)
-(global-set-key (kbd "S-<f9>") 'backward-kill-word)
-(global-set-key "\M-[1;5C" 'forward-word)
-(global-set-key "\M-[1;5D" 'backward-word)
-(global-set-key "\M-[1;5A" 'backward-paragraph)
-(global-set-key "\M-[1;5B" 'forward-paragraph)
-
-(global-set-key (kbd "C-x g") 'magit-status)
-
-(setq asm-comment-char ?\#)
-
-;; simple regexp used to check the message. Tweak to your own need.
-;; (defvar my-message-attachment-regexp "\\([Ww]e send\\|[Ii] send\\|attach\\)")
-(defvar my-message-attachment-regexp "\\([Vv]edlagt\\|[Vv]edlegg\\)")
-;; the function that checks the message
-(defun my-message-check-attachment nil
-  "Check if there is an attachment in the message if I claim it."
-  (save-excursion
-    (message-goto-body)
-    (when (search-forward-regexp my-message-attachment-regexp nil t nil)
-      (message-goto-body)
-      (unless (or (search-forward "<#part" nil t nil)
-  		  (message-y-or-n-p
-                   "No attachment. Send the message ?" nil nil))
-  	(error "No message sent")))))
-;; check is done just before sending the message
-(add-hook 'message-send-hook 'my-message-check-attachment)
-
-(setq inferior-lisp-program (executable-find "sbcl"))
-
-;; (add-hook 'mail-mode-hook          'turn-on-auto-fill)
-;; (add-hook 'mail-mode-hook          'mail-abbrevs-setup)
-;; (setq mail-yank-prefix "> "
-;;       mail-signature "\n\n-- Eigil"
-;;       mail-default-headers "FCC: ~/SENT\n")
-
-;; (add-to-list 'auto-mode-alist '(".*mutt.*" . mail-mode))
-;; (setq mail-header-separator "")
-
-;; (add-hook 'mail-mode-hook
-;;           (lambda ()
-;;             (font-lock-add-keywords nil
-;;                                     '(("^[ \t]*>[ \t]*>[ \t]*>.*$"
-;;                                        (0 'mail-multiply-quoted-text-face))
-;;                                       ("^[ \t]*>[ \t]*>.*$"
-;;                                        (0 'mail-double-quoted-text-face))))))
-
-;; (defun mutt-mail-mode-hook ()
-;;   (flush-lines "^\\(> \n\\)*> -- \n\\(\n?> .*\\)*") ; kill quoted sigs
-;;   (not-modified)
-;;   (mail-text)
-;;   (setq make-backup-files nil))
-;; (or (assoc "mutt-" auto-mode-alist)
-;;     (setq auto-mode-alist (cons '("/tmp/mutt.*" . mail-mode) auto-mode-alist)))
-;; (add-hook 'mail-mode-hook 'mutt-mail-mode-hook)
-
-;; (require 'airline-themes)
-;; (load-theme 'airline-light)
-(require 'flycheck)
-(flycheck-define-checker proselint
-  "A linter for prose."
-  :command ("proselint" source-inplace)
-  :error-patterns
-  ((warning line-start (file-name) ":" line ":" column ": "
-            (id (one-or-more (not (any " "))))
-            (message (one-or-more not-newline)
-                     (zero-or-more "\n" (any " ") (one-or-more not-newline)))
-            line-end))
-  :modes (text-mode markdown-mode gfm-mode latex-mode))
-(add-to-list 'flycheck-checkers 'proselint)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
-(put 'TeX-narrow-to-group 'disabled nil)
-
-(eval-after-load 'outline
-  '(progn
-    (require 'outline-magic)
-    (define-key outline-minor-mode-map (kbd "<C-tab>") 'outline-cycle)))
-(add-hook 'LaTeX-mode-hook #'latex-extra-mode)
+(load custom-file :noerror)
